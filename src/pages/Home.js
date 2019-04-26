48caebf48bc71bb6f528b56640207404b6e95180
LizThrilla <elizabeth.tiller@gmail.com>
2019-04-26 14:49:51 -0400

    why butterfly

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,12 +2,14 @@ import React from 'react'
 import './Home.css'
 import Header from '../components/Header'
 import Service from '../components/Service'
+import Butterfly from '../components/Butterfly'
 
 const Home = () => {
     return (
         <div className="home">
             <Header />
             <Service />
+            <Butterfly />
         </div>
     )
 
