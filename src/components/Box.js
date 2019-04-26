41953e5a0245252f11f2ab667bf368cf2e3aa86b
LizThrilla <elizabeth.tiller@gmail.com>
2019-04-26 14:31:44 -0400

    size

--- src/components/Box.js
+++ src/components/Box.js
@@ -4,7 +4,7 @@ const Box = () => {
     return (
         <div className="box">
             <h4>Service</h4>
-            <h4>Trip on catnip show belly scream at teh bath but scamper yet groom forever, stretch tongue and leave it slightly out, blep as lick i the shoes yet chase the pig around the house. Purrrrrr pet right here, no not there, here, no fool, right here that other cat smells funny you should really give me all the treats because i smell the best and omg you finally got the right spot and i love you right now.</h4>
+            <h4>Purrrrrr pet right here, no not there, here, no fool, right here that other cat smells funny you should really give me all the treats because i smell the best and omg you finally got the right spot and i love you right now.</h4>
         </div>
     )
 }
