d83e5f7fd2bf6fd7b0edcbdcb49f2d05885856e9
LizThrilla <elizabeth.tiller@gmail.com>
2019-04-29 17:49:48 -0400

    updated

--- src/components/Butterfly.js
+++ src/components/Butterfly.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Butterfly = () => {
     return (
-        <div>
+        <div className="butterfly">
             <h3>Why Butterfly?</h3>
             <p>
                 Ray Bradbury introduced the idea that the flap of a butterfly's wings. often called the "Butterfly Effect," could have a ripple effect on history in his 1952 short story, The Sound of Thunder. In 1961, Edward Norton Lorenz, an early proponent of chaos theory, noted that tiny variations in data could have a major effect on weather models and likened it to the flap of a butterfly's wing in one part of the world causing tornadoes in another part. John Tiller named his consulting firm Butterfly Financial Consultants because he believes that small actions can have major effects in the insurance world. John's experience, leadership, and creativity provide innovative insurance and reinsurance solutions for problems and clients of all sizes.
